3bcf582343cc30d6cd84e9785ee46ebc4e563862
Chris Burr <chrisburr@users.noreply.github.com>
2020-10-11 12:00:14 +0200

    Make cni directory before copying to iy

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ build_linux()
     find -type f -exec sed -i'' "s|/etc/cni/net\.d|$PREFIX/etc/cni/net\.d|g" {} \;
     ./build_linux.sh
 
+    mkdir -p "$PREFIX/lib/cni"
     cp -avf bin/* "$PREFIX/lib/cni"
 }
 
